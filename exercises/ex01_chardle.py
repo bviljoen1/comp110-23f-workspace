c6543db2c5e03a170c35f00867d2ad858d8c608e
bviljoen1 <bviljoen@unc.edu>
2023-09-15 16:32:14 -0400

    Progress on Exercise 1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -1,17 +1,14 @@
 """EX01 - Chardle - A cute step toward Wordle."""
 __author__ = "730579443"
 random_word: str = input("Enter a 5-character word: ")
-#if random word isnt 5 characters
 if len(random_word) != 5:
     print("Error: Word must contain 5 characters")
     exit()
-#if single character isnt 1 character
 single_character: str = input("Enter a single character: ")
 if len(single_character) != 1:
     print("Error: Character must be a single character.")
     exit()
 print("Searching for " + single_character + " in " + random_word)
-#I LOVE COMP 110
 
 character_sum: int = 0
 if random_word[0] == single_character:
@@ -30,12 +27,9 @@ if random_word[4] == single_character:
     print(single_character + " found at index 4")
     character_sum += 1
 
-#If no matches found
 if character_sum == 0:
     print("No instances of " + single_character + " found in " + random_word)
-#if 1 match ... instance singular tense
 if character_sum == 1:
     print(str(character_sum) + " instance of " + single_character + " found in " + random_word)
-#if more than 1 match... instances plural tense
 if character_sum >1:
     print(str(character_sum) + " instances of " + single_character + " found in " + random_word)
