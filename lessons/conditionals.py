d233022d9664aabddb7468b8f21ee987530d24d3
bviljoen1 <bviljoen@unc.edu>
2023-09-15 16:22:30 -0400

    Progress on Exercise 1

---
+++ lessons/conditionals.py
@@ -0,0 +1,8 @@
+"""Testing Conditionals with Low Card Example"""
+
+my_number_string: str = input("Guess a number: ")
+my_number: int = int(my_number_string)
+if my_number_string == int = 10
+    print("Right")
+else: 
+    print(input("Guess a number: ") + " is not correct")
---
+++ lessons/practice.py
@@ -0,0 +1,5 @@
+"""Practice for my own benefit"""
+
+user_name: str = input("What is your name, Gamer? ")
+print("Top of the mornin' to ya, " + user_name + "! how do you do?")
+print("you are so awesome and cool, " + user_name + "!")
