ad3a8c36ae3eb31005e7cd3168b786d33864267f
bviljoen1 <bviljoen@unc.edu>
2023-10-09 21:07:00 -0400

    Progress on Exercise 4

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -3,7 +3,7 @@ __author__ = "730579443"
 
 def all(list1: list[int], num1: int) -> bool:
     """Given a list of ints and a singualr int, will return true or false depending on whether the ints in the list are the same as the singular int."""
-    # Do the stuff
+    # Will check if the list exists, and whether every integer in the list is equal to the singular search integer.
     if len(list1) == 0:
         return False
     num_idx: int = 0
@@ -16,6 +16,8 @@ def all(list1: list[int], num1: int) -> bool:
 print(all([1, 1, 1], 1))
 
 def max(input: list[int]) -> int:
+    """Given a list of integers, it will return the integer of highest value."""
+    # Will make sure the list isn't empty, then is going to initialize the max to be the first integer. then check if integer 2 is bigger than the first, if so then the second integer becomes the new maximum and the loop repeats until the list runs out of values.
     if len(input) == 0:
         raise ValueError("max() arg is an empty List")
     max_num: int = input[0]
@@ -30,6 +32,8 @@ print(max([2, 2, 35]))
 
 
 def is_equal(lista: list[int], listb: list[int]) -> bool:
+    """Given two lists, the is_equal function will first check that they're the same length. If theyre not it returns False. After this it will make surethey're both set to the same index (equal_idx...)it will go through each index, comparing each list at that index, if at any point the two lists aren't equal it will exit and return false. if it gets through every index and they're all the same it will exit the while loop and return True."""
+    # Will check lengths are same then index each integer, then will return True or False depending on the outcome of the previous test.
     lengtha: int = len(lista)
     lengthb: int = len(listb)
     equal_idx: int = 0
